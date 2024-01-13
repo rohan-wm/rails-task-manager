e82713e4bdd4c54452142acb4f3aba9509d379ac
rohan martin <rohan1215@hotmail.com>
2024-01-13 14:20:48 +1100

    finished task app, needs to be pretty

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,42 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @tasks = Task.new(task_params)
+    @tasks.save # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_path(@tasks)
+  end
+
+  def edit
+    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @tasks = Task.find(params[:id])
+    @tasks.update(task_params)
+    redirect_to task_path(@tasks)
+  end
+
+  def destroy
+    @tasks = Task.find(params[:id])
+    @tasks.destroy
+    redirect_to tasks_path(@tasks)
+  end
 end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "index", to: "tasks#index", as: 'tasks'
+  get "index/new", to: "tasks#new", as: 'new_task'
+  get "index/:id", to: "tasks#show", as: 'task'
+  post "index", to: "tasks#create"
+  get "index/:id/edit", to: "tasks#edit", as: 'edit_task'
+  patch "index/:id", to: "tasks#update"
+  delete "index/:id", to: "tasks#destroy"
 end
